e254d36601936c00e35739c31476864cd177a6b2
iwnasFillippou <p17fill@ionio.gr>
2023-09-16 10:59:44 -0400

    commit

--- bb.sh
+++ bb.sh
@@ -20,18 +20,18 @@ global_variables() {
     global_software_version="2.10"
 
     # Blog title
-    global_title="My fancy blog"
+    global_title="A blog about Software Ionian University"
     # The typical subtitle for each blog
-    global_description="A blog about turtles and carrots"
+    global_description="A blog about software and tech at Ionian University"
     # The public base URL for this blog
     global_url="http://example.com/blog"
 
     # Your name
-    global_author="John Smith"
+    global_author="p2017107"
     # You can use twitter or facebook or anything for global_author_url
-    global_author_url="http://twitter.com/example" 
+#    global_author_url="http://twitter.com/example" 
     # Your email
-    global_email="john@smith.com"
+#    global_email="john@smith.com"
 
     # CC by-nc-nd is a good starting point, you can change this to "&copy;" for Copyright
     global_license="CC by-nc-nd"
